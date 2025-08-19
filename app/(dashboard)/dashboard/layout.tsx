6ee3446be7b67655bf9084b9dc8292c984fe88c0
hifjur rahman <hifjurr66@gmail.com>
2025-08-19 12:29:58 -0400

    make erewrewrewrwerin

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -14,14 +14,10 @@ export default function DashboardLayout({
   const pathname = usePathname();
 
   return (
-    <div className="flex flex-col min-h-[calc(100dvh-68px)] max-w-7xl mx-auto w-full">
-      {/* Mobile header */}
-
-      <div className="flex flex-1 overflow-hidden h-full">
-
-        {/* Main content */}
-<main className="flex-1 overflow-y-auto p-0 lg:p-4 h-full">{children}</main>
-      </div>
-    </div>
+   <div className="flex flex-col h-screen max-w-7xl mx-auto w-full">
+  <div className="flex flex-1 overflow-hidden">
+    <main className="flex-1 overflow-hidden">{children}</main>
+  </div>
+</div>
   );
 }

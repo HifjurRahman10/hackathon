44f1fc5411d3264fe30210c71673323d25aceb66
hifjur rahman <hifjurr66@gmail.com>
2025-08-26 23:46:45 -0400

    added random shit

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -45,6 +45,7 @@ export function Login({ mode = 'signin' }: { mode?: 'signin' | 'signup' }) {
             >
               Email
             </Label>
+            
             <div className="mt-1">
               <Input
                 id="email"

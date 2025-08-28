52379ef40d6ba69545ee8672d53e4baeec2f6c61
hifjur rahman <hifjurr66@gmail.com>
2025-08-27 23:08:17 -0400

    added random shit

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -28,7 +28,6 @@ export async function createCheckoutSession({
 }: {
   team: BillingTeam;
   priceId: string;
-  
 }) {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
   const session = await stripe.checkout.sessions.create({

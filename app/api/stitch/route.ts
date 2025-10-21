8e150bf2fdcdffc6b4a3646b3f8bf6d0cd7b08eb
hifjur rahman <hifjurr66@gmail.com>
2025-10-21 17:12:32 -0400

    new

--- app/api/stitch/route.ts
+++ app/api/stitch/route.ts
@@ -13,6 +13,7 @@ if (ffmpegStatic) {
   ffmpeg.setFfmpegPath(ffmpegStatic);
 }
 
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!

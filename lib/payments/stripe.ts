0040d10fcf13f1b389c76fff7ad3b00c14c1795c
hifjur rahman <hifjurr66@gmail.com>
2025-10-22 19:05:26 -0400

    :

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -5,7 +5,7 @@ import {
 } from '@/lib/db/queries';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil'
+  apiVersion: '2025-07-30.basil'
 });
 
 // Updated BillingTeam type to match your UUID schema

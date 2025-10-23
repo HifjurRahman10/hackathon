70ad156c27a492c0268cc6d4432b63c270c97668
hifjur rahman <hifjurr66@gmail.com>
2025-10-23 19:31:52 -0400

    new code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,5 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.POSTGRES_URL!,
-  },
+  connectionString: process.env.POSTGRES_URL!,
 } satisfies Config;
